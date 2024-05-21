d71ef1911446b3c4cc257dde268c8d0f7156e754
dpound@naver.com <dpound@naver.com>
2024-05-21 21:44:50 +0900

    LUBID 프로젝트 라이브 관련 어플리케이션 프로젝트 초반 세팅

---
+++ src/main/java/com/lubid/lubidlive/LubidLiveApplication.java
@@ -0,0 +1,13 @@
+package com.lubid.lubidlive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LubidLiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LubidLiveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lubid/lubidlive/LubidLiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lubid.lubidlive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LubidLiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
