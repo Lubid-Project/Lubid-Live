5502d363b97b66c10acbdf606697d7e709542351
dpound@naver.com <dpound@naver.com>
2024-05-22 22:05:49 +0900

    live 앱 시큐리티 추가

---
+++ src/main/java/com/lubid/lubidlive/config/AdvancedSecurityConfig.java
@@ -0,0 +1,42 @@
+package com.lubid.lubidlive.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+@EnableWebSecurity
+@RequiredArgsConstructor
+@Configuration
+public class AdvancedSecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf((csrf) -> csrf.ignoringRequestMatchers("/**")) // 모든 uri csrf 무시하기
+                .sessionManagement(sessionManagement ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )    // 세션을 사용하지 않으므로 STATELESS 설정
+                .authorizeHttpRequests(requests ->
+                        requests.requestMatchers("/", "/lubid-user/auth/**").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .build();
+    }
+
+}
